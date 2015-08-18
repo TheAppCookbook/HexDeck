8a10759978973ca39ab6bcd0e679e3731771d30e
Patrick Perini <pcperini@gmail.com>
2015-08-17 21:55:33 -0700

    touch pulse

--- Hex Deck/Hex Deck/TapView.swift
+++ Hex Deck/Hex Deck/TapView.swift
@@ -7,31 +7,53 @@
 //
 
 import UIKit
+import QuartzCore
 
 class TapView: UIView {
     // MARK: Properties
     private var centerView: UIView?
+    private var backgroundView: UIView?
+    
     var identifier: String = ""
     
     // MARK: Initializers
     override func layoutSubviews() {
         super.layoutSubviews()
+        self.layer.masksToBounds = true
+        
+        // Background View
+        self.backgroundView?.removeFromSuperview()
+        
+        self.backgroundView = UIView(frame: self.bounds)
+        self.backgroundView?.backgroundColor = self.backgroundColor
+        
+        let scaleAnimation:CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+        
+        scaleAnimation.duration = 1.0
+        scaleAnimation.repeatCount = HUGE
+        scaleAnimation.autoreverses = true
+        scaleAnimation.fromValue = 0.9
+        scaleAnimation.toValue = 0.5
+        
+        self.backgroundView?.layer.removeAllAnimations()
+        self.backgroundView?.layer.addAnimation(scaleAnimation, forKey: nil)
+        
+        self.backgroundView?.layer.cornerRadius = max(self.frame.width, self.frame.height) / 2.0
+        self.backgroundView?.layer.masksToBounds = true
+        
+        self.addSubview(self.backgroundView!)
+        
+        // Center View
         self.centerView?.removeFromSuperview()
         
-        self.centerView = UIView(frame: CGRectInset(self.bounds, self.bounds.width / 3.0, self.bounds.height / 3.0))
+        self.centerView = UIView(frame: CGRectInset(self.bounds, self.bounds.width / 4.0, self.bounds.height / 4.0))
         self.centerView?.backgroundColor = self.backgroundColor?.colorWithAlphaComponent(0.70)
         
-        self.addSubview(self.centerView!)
-    }
-    
-    // MARK: Drawing Handlers
-    override func drawRect(rect: CGRect) {
         self.centerView?.layer.cornerRadius = max(self.centerView!.frame.width, self.centerView!.frame.height) / 2.0
         self.centerView?.layer.masksToBounds = true
         
-        self.layer.cornerRadius = max(self.frame.width, self.frame.height) / 2.0
-        self.layer.masksToBounds = true
+        self.addSubview(self.centerView!)
         
-        super.drawRect(rect)
+        self.backgroundColor = nil
     }
 }
